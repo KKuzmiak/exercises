8335aeb7036e125f1b84fc29c9a79221fc65a536
Kinga <kinga_kolodziej@interia.pl>
2019-04-04 22:37:30 +0200

    Porzadki na repo

--- pandas_zapytanie.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-with open (r'/home/kinga/stosowanie_git/zapytania__sql_git.sql') as f:
-   zapytanie=f.read()
-
-import sqlite3
-conn=sqlite3.connect("/home/kinga/sqlite/oreilly_getting_started_with_sql-master/rexon_metals.db")
-df=pd.read_sql_query(zapytanie, conn)
-df.to_excel('/home/kinga/stosowanie_git/wynik_excel.xlsx',sheet_name='raport',index=False)
-df
